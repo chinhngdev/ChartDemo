74d850cb4a42289de217abcf3522d3ce1305c745
ChinhNT <129706624+chinhgotrack@users.noreply.github.com>
2024-05-21 18:03:16 +0700

    update line chart

--- ChartDemo/LineChartViewController.swift
+++ ChartDemo/LineChartViewController.swift
@@ -9,27 +9,35 @@ import UIKit
 import DGCharts
 
 class LineChartViewController: UIViewController {
-    
+
+    // MARK: - Views
+    private lazy var chartLimitLine: ChartLimitLine = {
+        let line = ChartLimitLine()
+        line.limit = 100
+        line.label = "100 goals"
+        line.lineWidth = 1.0
+        line.lineColor = .systemRed
+        line.valueTextColor = .black
+        line.lineDashLengths = [3, 3]
+        return line
+    }()
+
     private lazy var lineChartView: LineChartView = {
         let chart = LineChartView()
+        chart.leftAxis.axisMaximum = 120
+        chart.leftAxis.addLimitLine(chartLimitLine)
         return chart
     }()
     
-    let players = [
-        "Ozil",
-        "Ramsey",
-        "Laca",
-        "Auba",
-        "Xhaka",
-        "Torreira"
-    ]
-    let goals = [6, 8, 26, 30, 8, 10]
-    
+    // MARK: - Properties
+    let players = ["Ozil", "Ramsey", "Laca", "Auba", "Xhaka", "Torreira", "Chinh"]
+    let goals = [6, 10, 10, 30, 26, 10, 100]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupLayout()
-        customizeChart(dataPoints: players, values: goals.map{ Double($0) })
+        customizeChart(players: players, goals: goals.map{ Double($0) })
     }
     
     private func setupLayout() {
@@ -37,16 +45,18 @@ class LineChartViewController: UIViewController {
         lineChartView.snp.makeConstraints {
             $0.height.equalTo(400)
             $0.leading.trailing.centerY.equalToSuperview()
+//            $0.edges.equalToSuperview().inset(50)
         }
     }
     
-    func customizeChart(dataPoints: [String], values: [Double]) {
+    func customizeChart(players: [String], goals: [Double]) {
         var dataEntries: [ChartDataEntry] = []
-        for i in 0..<dataPoints.count {
-          let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+        for i in 0..<players.count {
+          let dataEntry = ChartDataEntry(x: Double(i), y: goals[i])
           dataEntries.append(dataEntry)
         }
         let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: "LineChart")
+        lineChartDataSet.mode = .horizontalBezier
         let lineChartData = LineChartData(dataSet: lineChartDataSet)
         lineChartView.data = lineChartData
     }
